a25bc51fa11938b8ffd35df0e10ef8cb408176eb
Marcus <mpt789@gmail.com>
2020-05-21 23:59:07 -0700

    added HTML inside js to change page layout when start button is clicked. Also added clear interval function for when timer gets to zero.

--- script.js
+++ script.js
@@ -1,33 +1,77 @@
-      // Variables to be used:
-      // The array of questions for our quiz game.
-      var questions = [
-        { question: "The sky is blue.", answers: ["True", "False"], correctAnswer: "True",},
-        { question: "There are 365 days in a year.", answers: ["True", "False"], correctAnswer: "True",},
-        { question: "There are 16 ounces in a pound.", answers: ["True", "False"], correctAnswer: "True",},
-        { question: "The Declaration of Independence was created in 1745.", answers: ["True", "False"], correctAnswer: "False",},
-        { 
-        question: "Bananas are not vegetables.", 
-        answers: ["True", "False"], 
-        correctAnswer: "True",
-      },
-      ];
-      //variables created for code below
-      var questionIndex = 0;
-      var time = 100;
-      var $timer = document.querySelector("#timer");
       
-      //decrements time from time variable created above
-      //TODO: add stop timer conditional
-      //TODO: add clearInterval 
-      //TODO: restart quiz and add name for high score
-      //TODO: add save info function
-      $timer.textContent = time;
-      var timer = setInterval(function () {
-        time--;
-        $timer.textContent = time;                                            
-      }, 1000);
+
+      var questionContainer = document.getElementById("question-container");
+      var startButton = document.createElement("button");
+      var question = document.getElementById("question");
+      var answers = document.getElementById("answers");
+      questionContainer.appendChild(startButton);
+      startButton.innerText = ("Start");
+      startButton.setAttribute("class", "bg-danger pl-5 pr-5 pt-3 pb-3 rounded");
       
-      console.log(questions[4].answers[1])
+      
+      startButton.addEventListener("click", function(){
+        
+        var body = document.getElementById("body");
+        var containerFluid = document.getElementById("container-fluid");
+        var divLeft = document.getElementById("divLeft");
+        var divMid = document.getElementById("divMid");
+        var divRight = document.getElementById("divRight");
+        var h1 = document.getElementById("h1");
+        var questionContainer = document.getElementById("question-container");
+
+        body.setAttribute("class", "bg-danger");
+        containerFluid.setAttribute("class", "container-fluid");
+        divLeft.setAttribute("class", "col-md-3");
+        divMid.setAttribute("class", "col-md-6 text-center pt-5 pb-100 bg-secondary");
+        h1.setAttribute("class", "text-center");
+        questionContainer.setAttribute("class", "text-center")
+        divRight.setAttribute("class", "col-md-3");
+        
+        h1.innerText = ("Go!");
+        questionContainer.innerHTML = ("")
+        questionContainer.appendChild(question);
+        questionContainer.appendChild(answers);
+
+
+        
+        // Variables to be used:
+        // The array of questions for our quiz game.
+        var questions = [
+          { question: "The sky is blue.", answers: ["True", "False"], correctAnswer: "True",},
+          { question: "There are 365 days in a year.", answers: ["True", "False"], correctAnswer: "True",},
+          { question: "There are 16 ounces in a pound.", answers: ["True", "False"], correctAnswer: "True",},
+          { question: "The Declaration of Independence was created in 1745.", answers: ["True", "False"], correctAnswer: "False",},
+          { 
+            question: "Bananas are not vegetables.", 
+            answers: ["True", "False"], 
+            correctAnswer: "True",
+          },
+        ];
+        
+        //TODO: restart quiz and add name for high score
+        //TODO: add save info function
+        
+        
+        //variables created for timer
+        var questionIndex = 0;
+        var timerDiv = document.querySelector("#timer");
+        var time = 100;
+        //decrements time from time variable created above
+        
+        timerDiv.textContent = time;
+        
+        var timer = setInterval(function () {
+          time--;
+          timerDiv.textContent = time;
+          
+          //added stop timer conditional
+          //added clearInterval 
+            if(time === 0) {
+              clearInterval(timer);
+            }                                         
+          }, 1000);
+      
+      
       function renderQuestion() {
         var question = questions[questionIndex];
         var $question = document.querySelector("#question");
@@ -42,12 +86,13 @@
           $answers.append($btn);
         }
       };
-
-      document
-       .querySelector("#answers")
-       .addEventListener("click", function(e) {
-         if (!e.target.matches("button")) return;
-         questionIndex++;
-         renderQuestion();
-        });
+      
+      document.querySelector("#answers").addEventListener("click", function(e) {
+        if (!e.target.matches("button")) return;
+        questionIndex++;
+        renderQuestion();
+      });
       renderQuestion();
+      
+    });
+    
