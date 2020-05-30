8710b8222fc89d1cfb1ee3641b0778686650021d
Marcus <mpt789@gmail.com>
2020-05-30 14:12:08 -0700

    formatted docs and shnge init message

--- script.js
+++ script.js
@@ -1,98 +1,107 @@
-      
+let questionContainer = document.getElementById("question-container");
+let startButton = document.createElement("button");
+let question = document.getElementById("question");
+let answers = document.getElementById("answers");
+questionContainer.appendChild(startButton);
+startButton.innerText = "Start";
+startButton.setAttribute("class", "bg-danger pl-5 pr-5 pt-3 pb-3 rounded h2");
 
-      var questionContainer = document.getElementById("question-container");
-      var startButton = document.createElement("button");
-      var question = document.getElementById("question");
-      var answers = document.getElementById("answers");
-      questionContainer.appendChild(startButton);
-      startButton.innerText = ("Start");
-      startButton.setAttribute("class", "bg-danger pl-5 pr-5 pt-3 pb-3 rounded");
-      
-      
-      startButton.addEventListener("click", function(){
-        
-        var body = document.getElementById("body");
-        var containerFluid = document.getElementById("container-fluid");
-        var divLeft = document.getElementById("divLeft");
-        var divMid = document.getElementById("divMid");
-        var divRight = document.getElementById("divRight");
-        var h1 = document.getElementById("h1");
-        var questionContainer = document.getElementById("question-container");
+startButton.addEventListener("click", function () {
+  let body = document.getElementById("body");
+  let containerFluid = document.getElementById("container-fluid");
+  let divLeft = document.getElementById("divLeft");
+  let divMid = document.getElementById("divMid");
+  let divRight = document.getElementById("divRight");
+  let h1 = document.getElementById("h1");
+  let questionContainer = document.getElementById("question-container");
 
-        body.setAttribute("class", "bg-danger");
-        containerFluid.setAttribute("class", "container-fluid");
-        divLeft.setAttribute("class", "col-md-3");
-        divMid.setAttribute("class", "col-md-6 text-center pt-5 pb-100 bg-secondary");
-        h1.setAttribute("class", "text-center");
-        questionContainer.setAttribute("class", "text-center")
-        divRight.setAttribute("class", "col-md-3");
-        
-        h1.innerText = ("Go!");
-        questionContainer.innerHTML = ("")
-        questionContainer.appendChild(question);
-        questionContainer.appendChild(answers);
+  body.setAttribute("class", "bg-danger");
+  containerFluid.setAttribute("class", "container-fluid");
+  divLeft.setAttribute("class", "col-md-3");
+  divMid.setAttribute("class", "col-md-6 text-center pt-5 bg-secondary");
+  divMid.setAttribute("style", "position:fixed; bottom:0px; height: 100%;");
+  h1.setAttribute("class", "text-center");
+  questionContainer.setAttribute("class", "text-center");
+  divRight.setAttribute("class", "col-md-3");
 
+  h1.innerText = "Go!";
+  questionContainer.innerHTML = "";
+  questionContainer.appendChild(question);
+  questionContainer.appendChild(answers);
 
-        
-        // Variables to be used:
-        // The array of questions for our quiz game.
-        var questions = [
-          { question: "The sky is blue.", answers: ["True", "False"], correctAnswer: "True",},
-          { question: "There are 365 days in a year.", answers: ["True", "False"], correctAnswer: "True",},
-          { question: "There are 16 ounces in a pound.", answers: ["True", "False"], correctAnswer: "True",},
-          { question: "The Declaration of Independence was created in 1745.", answers: ["True", "False"], correctAnswer: "False",},
-          { 
-            question: "Bananas are not vegetables.", 
-            answers: ["True", "False"], 
-            correctAnswer: "True",
-          },
-        ];
-        
-        //TODO: restart quiz and add name for high score
-        //TODO: add save info function
-        
-        
-        //variables created for timer
-        var questionIndex = 0;
-        var timerDiv = document.querySelector("#timer");
-        var time = 100;
-        //decrements time from time variable created above
-        
-        timerDiv.textContent = time;
-        
-        var timer = setInterval(function () {
-          time--;
-          timerDiv.textContent = time;
-          
-          //added stop timer conditional
-          //added clearInterval 
-            if(time === 0) {
-              clearInterval(timer);
-            }                                         
-          }, 1000);
-      
-      
-      function renderQuestion() {
-        var question = questions[questionIndex];
-        var $question = document.querySelector("#question");
-        var $answers = document.querySelector("#answers");
-        $question.textContent = question.question;
-        $answers.innerHTML = "";
-        
-        for (var i = 0; i < question.answers.length; i++) {
-          var $btn = document.createElement("button");
-          $btn.textContent = question.answers[i];
-          $btn.setAttribute("class", "btn btn-primary ml-3 mt-3 pl-5 pr-5");
-          $answers.append($btn);
-        }
-      };
-      
-      document.querySelector("#answers").addEventListener("click", function(e) {
-        if (!e.target.matches("button")) return;
-        questionIndex++;
-        renderQuestion();
-      });
-      renderQuestion();
-      
-    });
-    
+  // Variables to be used:
+  // The array of questions for our quiz game.
+  let questions = [
+    {
+      question: "The sky is blue.",
+      answers: ["True", "False"],
+      correctAnswer: "True",
+    },
+    {
+      question: "There are 365 days in a year.",
+      answers: ["True", "False"],
+      correctAnswer: "True",
+    },
+    {
+      question: "There are 16 ounces in a pound.",
+      answers: ["True", "False"],
+      correctAnswer: "True",
+    },
+    {
+      question: "The Declaration of Independence was created in 1745.",
+      answers: ["True", "False"],
+      correctAnswer: "False",
+    },
+    {
+      question: "Bananas are not vegetables.",
+      answers: ["True", "False"],
+      correctAnswer: "True",
+    },
+  ];
+
+  //TODO: add effect for correct or wrong button click
+  //TODO: add score count
+  //TODO: restart quiz and add name for high score
+  //TODO: add save info function
+
+  //variables created for timer
+  let questionIndex = 0;
+  let timerDiv = document.querySelector("#timer");
+  let time = 100;
+  //decrements time from time variable created above
+
+  timerDiv.textContent = time;
+
+  let timer = setInterval(function () {
+    time--;
+    timerDiv.textContent = time;
+
+    //added stop timer conditional
+    //added clearInterval
+    if (time === 0) {
+      clearInterval(timer);
+    }
+  }, 1000);
+
+  function renderQuestion() {
+    let question = questions[questionIndex];
+    let $question = document.querySelector("#question");
+    let $answers = document.querySelector("#answers");
+    $question.textContent = question.question;
+    $answers.innerHTML = "";
+
+    for (let i = 0; i < question.answers.length; i++) {
+      let $btn = document.createElement("button");
+      $btn.textContent = question.answers[i];
+      $btn.setAttribute("class", "btn btn-primary ml-3 mt-3 pl-5 pr-5");
+      $answers.append($btn);
+    }
+  }
+
+  document.querySelector("#answers").addEventListener("click", function (e) {
+    if (!e.target.matches("button")) return;
+    questionIndex++;
+    renderQuestion();
+  });
+  renderQuestion();
+});
